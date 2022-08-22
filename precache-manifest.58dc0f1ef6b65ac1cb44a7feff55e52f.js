b5f39d6e47daaff9892d00a346fa65414af6515c
Gulsher Ahmed <gulsherahmed@Gulshers-MacBook-Pro.local>
2022-08-22 23:38:05 +0530

    Updates

---
+++ precache-manifest.58dc0f1ef6b65ac1cb44a7feff55e52f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdeb630f881e9e640b7f",
+    "url": "/robo-redux/static/css/main.6feda9f6.chunk.css"
+  },
+  {
+    "revision": "bdeb630f881e9e640b7f",
+    "url": "/robo-redux/static/js/main.bdeb630f.chunk.js"
+  },
+  {
+    "revision": "f12bf432d1b1b25fb939",
+    "url": "/robo-redux/static/js/1.f12bf432.chunk.js"
+  },
+  {
+    "revision": "cddf46e9d7a5804ad3ec",
+    "url": "/robo-redux/static/css/2.a5e48d15.chunk.css"
+  },
+  {
+    "revision": "cddf46e9d7a5804ad3ec",
+    "url": "/robo-redux/static/js/2.cddf46e9.chunk.js"
+  },
+  {
+    "revision": "dff3011e112b9fdd5d39",
+    "url": "/robo-redux/static/js/runtime~main.dff3011e.js"
+  },
+  {
+    "revision": "fd6f8aa78a8cd1daa17431bc30369e65",
+    "url": "/robo-redux/index.html"
+  }
+];
